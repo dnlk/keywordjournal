98b1b4cd4a829a081338942b2b8578691831a21f
Daniel Kinney <kinney.daniel.j@gmail.com>
2016-01-09 20:13:49 -0500

    Set up initial DB schema

---
+++ db_schema/db_schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE keyword (
+	id INTEGER PRIMARY KEY,
+	word TEXT
+);
+
+CREATE TABLE user (
+	id INTEGER PRIMARY KEY,
+	email TEXT,
+	password TEXT
+);
+
+CREATE TABLE user_keyword (
+	id INTEGER PRIMARY KEY,
+	keyword_id INTEGER,
+	user_id INTEGER
+);
+
+CREATE TABLE user_keyword_arg (
+	id INTEGER PRIMARY KEY,
+	user_keyword_id INTEGER,
+	param_name TEXT
+);
+
+CREATE TABLE post (
+	id INTEGER PRIMARY KEY,
+	user_id INTEGER,
+	text TEXT,
+	header TEXT,
+	datetime DATETIME
+);
+
+CREATE TABLE user_keyword_posted (
+	id INTEGER PRIMARY KEY,
+	user_keyword_id INTEGER,
+	post_id INTEGER
+);
+
+CREATE TABLE user_keyword_posted_arg (
+	id INTEGER PRIMARY KEY,
+	user_keyword_posted_id INTEGER,
+	arg_id INTEGER,
+	param_value TEXT
+);
+
+CREATE TABLE user_session (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    created DATETIME
+);
