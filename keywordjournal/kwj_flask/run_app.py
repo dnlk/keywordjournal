b5323cc427cc604177514f2bfaa725e3282a5371
Daniel Kinney <kinney.daniel.j@gmail.com>
2016-06-22 20:56:59 -0400

    Make default server listen globally

--- keywordjournal/kwj_flask/run_app.py
+++ keywordjournal/kwj_flask/run_app.py
@@ -5,4 +5,4 @@ from keywordjournal.kwj_flask import app
 import keywordjournal.kwj_flask.request_handlers
 
 if __name__ == '__main__':
-    app.app.run(debug=True)
+    app.app.run(host='0.0.0.0', debug=True)
